fffaa6849ca34a21f0de1933149bbeed4247ce70
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-26 09:46:26 +0500

    Index05.py

--- Index05.py
+++ Index05.py
@@ -6,4 +6,22 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    x=s[0]
+    x1=s[1]
+    x2=s[2]
+    x3=s[3]
+    x4=s[4]
+
+    a=0
+    if x.isdigit():
+        a+=1
+    if x1.isdigit():
+        a+=1
+    if x2.isdigit():
+        a+=1
+    if x3.isdigit():
+        a+=1
+    if x4.isdigit():
+        a+=1
+    return a
+print(main("32x3z"))
